72c8830399c536b42826b5ebd7406c8846d34cec
Mert Aktaş <mertaktas231@gmail.com>
2021-08-11 21:18:59 +0300

    Home Page Updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,83 @@
 <template>
-  <div class="w-full min-h-screen h-full flex flex-wrap items-center justify-center bg-teal-100 p-8">
-    <Footer />
+  <div class="bg-gray-200 font-source-sans-pro antialiased text-gray-900 leading-normal">
+    <div class="flex flex-col items-center min-h-screen border-t-8 border-teal-500 bg-image" style="">
+      <div class="mt-24 mb-4">
+        <svg class="h-48" viewBox="0 0 90 90" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><circle id="b" cx="40" cy="40" r="40"/><filter x="-8.8%" y="-6.2%" width="117.5%" height="117.5%" filterUnits="objectBoundingBox" id="a"><feOffset dy="2" in="SourceAlpha" result="shadowOffsetOuter1"/><feGaussianBlur stdDeviation="2" in="shadowOffsetOuter1" result="shadowBlurOuter1"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0" in="shadowBlurOuter1"/></filter><linearGradient x1="0%" y1="0%" y2="100%" id="c"><stop stop-color="#2383AE" offset="0%"/><stop stop-color="#6DD7B9" offset="100%"/></linearGradient></defs><g fill="none" fill-rule="evenodd"><g transform="translate(5 5)"><use fill="#000" filter="url(#a)" xlink:href="#b"/><use fill="#FFF" xlink:href="#b"/></g><path d="M25.6 33.92C27.52 26.24 32.32 22.4 40 22.4c11.52 0 12.96 8.64 18.72 10.08 3.84.96 7.2-.48 10.08-4.32-1.92 7.68-6.72 11.52-14.4 11.52-11.52 0-12.96-8.64-18.72-10.08-3.84-.96-7.2.48-10.08 4.32zM11.2 51.2c1.92-7.68 6.72-11.52 14.4-11.52 11.52 0 12.96 8.64 18.72 10.08 3.84.96 7.2-.48 10.08-4.32-1.92 7.68-6.72 11.52-14.4 11.52-11.52 0-12.96-8.64-18.72-10.08-3.84-.96-7.2.48-10.08 4.32z" fill="url(#c)" transform="translate(5 5)"/></g></svg>
+      </div>
+      <h1 class="text-3xl font-bold mb-10">Tailwind CSS Examples</h1>
+      <div class="flex flex-wrap items-center justify-between w-3/5 mx-auto mb-10">
+      <!-- start item pinterest -->
+        <div class="w-64 flex items-center mx-auto mb-8 pr-8">
+          <div class="mr-4">
+            <a href="/pinterest" target="_blank">
+            <svg width="80" height="98" viewBox="0 0 256 256" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid">
+              <g>
+                  <path d="M0,128.002 C0,180.416 31.518,225.444 76.619,245.241 C76.259,236.303 76.555,225.573 78.847,215.848 C81.308,205.457 95.317,146.1 95.317,146.1 C95.317,146.1 91.228,137.927 91.228,125.848 C91.228,106.879 102.222,92.712 115.914,92.712 C127.557,92.712 133.182,101.457 133.182,111.929 C133.182,123.633 125.717,141.14 121.878,157.355 C118.671,170.933 128.686,182.008 142.081,182.008 C166.333,182.008 182.667,150.859 182.667,113.953 C182.667,85.899 163.772,64.901 129.405,64.901 C90.577,64.901 66.388,93.857 66.388,126.201 C66.388,137.353 69.676,145.217 74.826,151.307 C77.194,154.104 77.523,155.229 76.666,158.441 C76.052,160.796 74.642,166.466 74.058,168.713 C73.206,171.955 70.579,173.114 67.649,171.917 C49.765,164.616 41.436,145.031 41.436,123.015 C41.436,86.654 72.102,43.054 132.918,43.054 C181.788,43.054 213.953,78.418 213.953,116.379 C213.953,166.592 186.037,204.105 144.887,204.105 C131.068,204.105 118.069,196.635 113.616,188.15 C113.616,188.15 106.185,217.642 104.611,223.337 C101.897,233.206 96.585,243.07 91.728,250.758 C103.24,254.156 115.401,256.007 128.005,256.007 C198.689,256.007 256.001,198.698 256.001,128.002 C256.001,57.309 198.689,0 128.005,0 C57.314,0 0,57.309 0,128.002 Z" fill="#CB1F27"></path>
+              </g>
+            </svg>
+            </a>
+          </div>
+          <div class="leading-tight">
+            <div>
+              <a href="/pinterest" target="_blank" class="text-lg font-semibold hover:text-gray-600">Pinterest</a>
+            </div>
+          </div>
+        </div> <!-- end item pinterest -->
+        <!-- start item twitter -->
+        <div class="w-64 flex items-center mx-auto mb-8 pr-8">
+          <div class="mr-4">
+            <a href="/twitter" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" width="80" height="98" viewBox="0 0 98.488 80.021"><path fill="#03a9f4" d="M98.488,57.473a42.1,42.1,0,0,1-11.634,3.189A20.078,20.078,0,0,0,95.736,49.5a40.35,40.35,0,0,1-12.8,4.887A20.189,20.189,0,0,0,48.007,68.2a20.79,20.79,0,0,0,.468,4.6A57.15,57.15,0,0,1,6.857,51.681a20.2,20.2,0,0,0,6.2,26.986A19.94,19.94,0,0,1,3.939,76.18V76.4A20.284,20.284,0,0,0,20.116,96.241a20.152,20.152,0,0,1-5.294.665A17.853,17.853,0,0,1,11,96.561a20.383,20.383,0,0,0,18.867,14.065,40.57,40.57,0,0,1-25.034,8.612A37.819,37.819,0,0,1,0,118.96a56.843,56.843,0,0,0,30.974,9.061c37.154,0,57.468-30.777,57.468-57.455,0-.893-.031-1.754-.074-2.61A40.28,40.28,0,0,0,98.488,57.473Z" transform="translate(0 -48)"/></svg>
+            </a>
+          </div>
+          <div class="leading-tight">
+            <div>
+              <a href="/twitter" target="_blank" class="text-lg font-semibold hover:text-gray-600">Twitter</a>
+            </div>
+          </div>
+        </div> <!-- end item twitter -->
+        <!-- start item uidesigndaily -->
+        <div class="w-64 flex items-center mx-auto mb-8 pr-8">
+          <div class="mr-4">
+            <a href="/designdaily" target="_blank">
+            <svg width="80" height="98" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M2.21 7.73211C7.45202 -1.34735 21.3085 -1.78871 30.388 3.45332C39.4674 8.69534 42.5783 20.3052 37.3362 29.3846C32.0942 38.4641 19.8911 42.7835 10.8117 37.5414C1.73224 32.2994 -3.03203 16.8116 2.21 7.73211ZM22.0443 13.3232C21.6716 13.3232 21.376 13.4357 21.1575 13.6606C20.9702 13.8534 20.8631 14.1005 20.8364 14.4019L20.8297 14.5571V21.652C20.8297 22.8088 20.5534 23.6892 20.0007 24.2933C19.448 24.8974 18.6447 25.1995 17.5907 25.1995C16.5239 25.1995 15.7142 24.9006 15.1615 24.303C14.6513 23.7513 14.3766 22.9559 14.3374 21.9169L14.3325 21.652V14.5571C14.3325 14.1844 14.2232 13.8855 14.0047 13.6606C13.7862 13.4357 13.4906 13.3232 13.1178 13.3232C12.7451 13.3232 12.4495 13.4357 12.231 13.6606C12.0437 13.8534 11.9367 14.1005 11.9099 14.4019L11.9032 14.5571V21.5171C11.9032 23.3679 12.3852 24.7753 13.3492 25.7393C14.3132 26.7033 15.727 27.1853 17.5907 27.1853C19.4416 27.1853 20.849 26.7001 21.813 25.7297C22.7202 24.8163 23.2006 23.5188 23.2539 21.8369L23.2589 21.5171V14.5571C23.2589 14.1844 23.1497 13.8855 22.9312 13.6606C22.7127 13.4357 22.4171 13.3232 22.0443 13.3232ZM27.3462 13.3232C26.9735 13.3232 26.6747 13.4357 26.4497 13.6606C26.2569 13.8534 26.1468 14.1005 26.1192 14.4019L26.1123 14.5571V25.8743C26.1123 26.247 26.2248 26.5491 26.4497 26.7804C26.6747 27.0118 26.9735 27.1274 27.3462 27.1274C27.7318 27.1274 28.0371 27.0118 28.262 26.7804C28.4548 26.5821 28.565 26.3319 28.5925 26.0297L28.5994 25.8743V14.5571C28.5994 14.1844 28.4869 13.8855 28.262 13.6606C28.0371 13.4357 27.7318 13.3232 27.3462 13.3232Z" fill="#242E4C"/>
+            </svg>
+            </a>
+          </div>
+          <div class="leading-tight">
+            <div>
+              <a href="/designdaily" target="_blank" class="text-lg font-semibold hover:text-gray-600">Ui Design Daily</a>
+            </div>
+          </div>
+        </div> <!-- end item uidesigndaily -->
+
+
+      </div>
+
+      <div class="mb-2">
+        Made by <a href="https://github.com/mertaktas" class="text-blue-600 hover:text-blue-800">Mert Aktaş</a> &middot; <a href="https://github.com/mertaktas" class="text-blue-600 hover:text-blue-800">@mertaktas</a>
+      </div>
+
+    </div>
+
+    <a href="https://github.com/mertaktas/Tailwindcss-Examples" target="_blank" class="github-corner" aria-label="View source on Github">
+      <svg width="80" height="80" viewBox="0 0 250 250" style="fill:#37B2AB; color:#fff; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path>
+      </svg>
+    </a>
   </div>
 
+
 </template>
+
 <script>
-import Footer from "@/components/Footer.vue"
 export default {
-  components: {
-    Footer,
-  }
 }
 </script>
+
 <style>
+  .bg-image{
+    background-image: url(https://tailwind-v1-examples.netlify.app/_nuxt/img/496de04.png);
+  }
+  .github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}
 </style>
